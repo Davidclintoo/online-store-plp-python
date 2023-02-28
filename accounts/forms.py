a8b67a22bbd94a4d469b95fd19304efc7f24dc29
David Onyango <clintoodavi01@gmail.com>
2023-02-28 16:03:17 +0300

    new changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,30 +11,3 @@ class CustomerSignUpForm(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email')
         
-# class ProductCreateForm(forms.ModelForm):
-#     image = forms.ImageField()
-#     class Meta:
-#         model = Products
-#         fields = ('category', 'name', 'price', 'description', 'image', 'stock', 'available', 'slug')
-
-#     @property
-#     def imageURL(self):
-#         try:
-#             url = self.image.url
-#         except:
-#             url = ''
-#         return url
-
-# class ClientSignUpForm(UserCreationForm):
-#     first_name = forms.CharField(max_length=100, required=True)
-#     last_name = forms.CharField(max_length=100, required=True)
-#     email = forms.EmailField(max_length=250, help_text='eg. youremail@gmail.com')
-#     id_number = forms.CharField(max_length=13, required=True)
-#     phone = forms.CharField(max_length=15, required=True)
-#     business_name = forms.CharField(max_length=50, required=True)
-#     enterprise_number = forms.CharField(max_length=50, required=True)
-#     courier_service = forms.CharField(max_length=10)
-
-#     class Meta:
-#         model = User
-#         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'id_number', 'phone', 'business_name', 'enterprise_number', 'courier_service')
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,13 +4,5 @@ from. import views
 urlpatterns = [
     path('login/', views.login, name= 'Customer-login'),
     path('register/', views.register, name= 'Customer-signup'),
-    # path('logout/', views.customer_logout, name="customer_logout"),
-    # path('order_history/', views.orderHistory, name="order_history"),
-    # path('portal/', views.client_portal, name='portal'),
-    # path('portal1/', views.portal_admin, name='admin-portal'),
-    # path('add-products/', views.add_product, name="add-products"),
-    # path('product-list/', views.products, name="product-list"),
-    # path('docs/', views.docs, name="docs"),
-    # path('portal_login/', views.client_login, name='client_login'),
-    # path('portal_register/', views.client_register, name="client_register")
+
 ]
